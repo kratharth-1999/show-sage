092cef134ce4c92b3bf56acbf0ae5df455ac8582
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 18:38:39 +0530

    Added zod validation for Sign up form cause why not

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
+import z from "zod";
+
+const validateSignUpFormSchema = z
+    .object({
+        name: z.string().min(1, "Full name is required"),
+        email: z
+            .string()
+            .email("Email is not valid")
+            .min(1, "Email is required"),
+        password: z.string().min(8, "Password must have atleast 8 characters"),
+        confirmPassword: z
+            .string()
+            .min(8, "Password must have atleast 8 characters"),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
 
 const SignUpForm = ({ toggleSignInForm }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
+    const [errors, setErrors] = useState({});
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email, password);
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        try {
+            const validatedForm = validateSignUpFormSchema.parse(data);
+            setErrors({});
+            console.log(validatedForm);
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                const formattedErrors = err.errors.reduce((acc, error) => {
+                    const fieldName = error.path[0];
+                    acc[fieldName] = error.message;
+                    return acc;
+                }, {});
+                setErrors(formattedErrors);
+            }
+        }
     };
 
     return (
@@ -24,13 +55,17 @@ const SignUpForm = ({ toggleSignInForm }) => {
                         Full Name
                     </label>
                     <input
-                        type="name"
+                        type="text"
+                        name="name"
                         id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
                         placeholder="Enter your full name"
-                        className="w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500"
+                        className={`w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500 ${
+                            errors.name ? "ring-2 ring-red-500" : "ring-0"
+                        }`}
                     />
+                    {errors.name && (
+                        <p className="text-red-400 font-bold">{errors.name}</p>
+                    )}
                 </div>
                 <div className="space-y-2">
                     <label
@@ -41,12 +76,16 @@ const SignUpForm = ({ toggleSignInForm }) => {
                     </label>
                     <input
                         type="email"
+                        name="email"
                         id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Enter your email"
-                        className="w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500"
+                        className={`w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500 ${
+                            errors.email ? "ring-2 ring-red-500" : "ring-0"
+                        }`}
                     />
+                    {errors.email && (
+                        <p className="text-red-400 font-bold">{errors.email}</p>
+                    )}
                 </div>
                 <div className="space-y-2">
                     <label
@@ -57,12 +96,42 @@ const SignUpForm = ({ toggleSignInForm }) => {
                     </label>
                     <input
                         type="password"
+                        name="password"
                         id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
                         placeholder="Enter your password"
-                        className="w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500"
+                        className={`w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500 ${
+                            errors.password ? "ring-2 ring-red-500" : "ring-0"
+                        }`}
                     />
+                    {errors.password && (
+                        <p className="text-red-400 font-bold">
+                            {errors.password}
+                        </p>
+                    )}
+                </div>
+                <div className="space-y-2">
+                    <label
+                        htmlFor="confirmPassword"
+                        className="text-sm font-medium text-white"
+                    >
+                        Confirm Password
+                    </label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        placeholder="Re enter your password"
+                        className={`w-full px-4 py-2 bg-black/20 text-white border border-gray-700 rounded-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-red-500 ${
+                            errors.confirmPassword
+                                ? "ring-2 ring-red-500"
+                                : "ring-0"
+                        }`}
+                    />
+                    {errors.confirmPassword && (
+                        <p className="text-red-400 font-bold">
+                            {errors.confirmPassword}
+                        </p>
+                    )}
                 </div>
                 <button
                     type="submit"
