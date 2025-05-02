65203f1196390b0465ae8385f0543a5c2775eb59
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 19:36:57 +0530

    Brought in firebase into the project

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "showsage-374e8.firebaseapp.com",
+    projectId: "showsage-374e8",
+    storageBucket: "showsage-374e8.firebasestorage.app",
+    messagingSenderId: "953008573880",
+    appId: "1:953008573880:web:447323695dff857960134d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
