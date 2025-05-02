8e86c60b3feb050d9f8b5fd6036041508d1f4b8b
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 19:37:17 +0530

    Added a loader svg

---
+++ src/components/Loader.jsx
@@ -0,0 +1,46 @@
+const Loader = (fill = "#FFF", height = 28) => {
+    return (
+        <svg
+            version="1.1"
+            id="L4"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 100 100"
+            enable-background="new 0 0 0 0"
+            xml:space="preserve"
+            style={{ width: "100%", height: height }}
+        >
+            <circle fill="#fff" stroke="none" cx="6" cy="50" r="10">
+                <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite"
+                    begin="0.1"
+                />
+            </circle>
+            <circle fill="#fff" stroke="none" cx="36" cy="50" r="10">
+                <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite"
+                    begin="0.2"
+                />
+            </circle>
+            <circle fill="#fff" stroke="none" cx="66" cy="50" r="10">
+                <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite"
+                    begin="0.3"
+                />
+            </circle>
+        </svg>
+    );
+};
+
+export default Loader;
