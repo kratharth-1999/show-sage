666109960009baef36076e3d4aab02c4568f6388
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 19:37:32 +0530

    Updated firebase to get auth globally

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "showsage-374e8.firebaseapp.com",
     projectId: "showsage-374e8",
     storageBucket: "showsage-374e8.firebasestorage.app",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
