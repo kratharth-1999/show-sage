7a6e0f81edc7ff8484ede3b5bba2436be5c799b1
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 15:11:26 +0530

    Added App Page as the root page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Body from "./components/Body";
+
 function App() {
-    return (
-        <h1 className="text-3xl font-bold text-green-800">
-            This is a new project
-        </h1>
-    );
+    return <Body />;
 }
 
 export default App;
