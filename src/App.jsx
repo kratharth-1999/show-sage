ca78508d0ae7cec17e4e2cccad28591b1b717f5c
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 19:54:36 +0530

    Brought in react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./store/appStore";
+import { ToastContainer } from "react-toastify";
 
 function App() {
     return (
         <Provider store={appStore}>
             <Body />
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
         </Provider>
     );
 }
