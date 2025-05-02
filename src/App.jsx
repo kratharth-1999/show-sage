4f910f758be695759e85adc1cd9f3d82fb15c570
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-02 12:43:24 +0530

    Added tailwind and tested with the boilerplate h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-    return <h1>This is a new project</h1>;
+    return (
+        <h1 className="text-3xl font-bold text-green-800">
+            This is a new project
+        </h1>
+    );
 }
 
 export default App;
