21e074887fe2dc80efe9fe753e199af2f23b56dc
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-03 16:34:01 +0530

    Added way to fetch now playing movies data (Verification)

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { useGet } from "../hooks/useGet";
+import { TMDB_MOVIE_BASE_URL } from "../utils/constants";
 
 const Browse = () => {
+    const { stateOfGetRequest, getData } = useGet();
+
+    useEffect(() => {
+        getData(TMDB_MOVIE_BASE_URL + "/now_playing?page=1");
+    }, []);
+
+    useEffect(() => {
+        if (!stateOfGetRequest.data) return;
+        console.log(stateOfGetRequest.data);
+    }, [stateOfGetRequest.data]);
+
     return (
         <div>
             <Header />
