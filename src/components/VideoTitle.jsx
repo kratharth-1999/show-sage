e6d4b18c97dfafe822e1757af308482b4fa1b30d
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-04 16:19:48 +0530

    Some more css adjustments on overlap of video with cards

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,7 +4,7 @@ const VideoTitle = ({ title, overview }) => {
     return (
         <div className="space-y-4">
             <h1 className="text-5xl font-bold">{title}</h1>
-            <p className="text-lg w-1/2 2xl:block hidden">{overview}</p>
+            <p className="text-lg w-1/2 lg:block hidden">{overview}</p>
             <div className="flex gap-x-4">
                 <button className="bg-white text-black py-2 px-6 rounded-md text-xl font-bold hover:bg-opacity-80">
                     Play
