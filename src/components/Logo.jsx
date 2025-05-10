486d7d9d215c832ee9e6b378736e5f42ef32adea
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-10 19:20:05 +0530

    Click on logo redirects to browse

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router";
+
 export const Logo = () => {
+    const navigate = useNavigate();
     return (
-        <h1 className="text-red-600 text-4xl font-extrabold tracking-tight">
+        <h1
+            className="text-red-600 text-4xl font-extrabold tracking-tight cursor-pointer"
+            onClick={() => navigate("/browse")}
+        >
             ShowSage
         </h1>
     );
